54030749ac1d52d9dc2bec2802d09dc5558cfe1b
JieSu <jiesue93@gmail.com>
2020-12-05 15:45:32 -0600

    finished showing cars' detail page

--- routes/cars.js
+++ routes/cars.js
@@ -1,24 +1,24 @@
 var express = require('express');
 var router = express.Router();
-// var Menus = require('../models/menus');
+// var cars = require('../models/cars');
 
 var monk = require('monk');
-var db = monk('localhost:27017/restaurant');
+var db = monk('localhost:27017/car');
 
 router.get('/', function(req, res) {
-	var collection = db.get('menus');
+	var collection = db.get('cars');
 	
-	collection.find({}, function(err, menus){
+	collection.find({}, function(err, cars){
 		if (err) throw err;
-	  	res.json(menus);
+	  	res.json(cars);
 	});
 });
 
 router.get('/:id', function(req, res) {
-	var collection = db.get('menus');
-	collection.findOne({ _id: req.params.id }, function(err, menus){
+	var collection = db.get('cars');
+	collection.findOne({ _id: req.params.id }, function(err, cars){
 		if (err) throw err;
-	  	res.json(menus);
+	  	res.json(cars);
 	});
 });
 
